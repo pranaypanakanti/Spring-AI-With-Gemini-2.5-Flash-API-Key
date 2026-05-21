c85576ce7fa9e87e1ead5b4f382c72242c4da2aa
panakantipranay <panakantipranay@gmail.com>
2026-05-21 16:53:07 +0530

    Remove configs from SpringAiApplication file

--- src/main/java/com/SpringAI/SpringAiApplication.java
+++ src/main/java/com/SpringAI/SpringAiApplication.java
@@ -1,26 +1,13 @@
 package com.SpringAI;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.google.genai.GoogleGenAiChatModel;
-import org.springframework.ai.openai.OpenAiChatModel;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAiApplication.class, args);
-	}
-
-	@Bean
-	public ChatClient openAiChatClient(OpenAiChatModel chatModel){
-		return ChatClient.create(chatModel);
-	}
-
-	@Bean
-	public ChatClient geminiAiChatClient(GoogleGenAiChatModel chatModel){
-		return ChatClient.create(chatModel);
 	}
 }
