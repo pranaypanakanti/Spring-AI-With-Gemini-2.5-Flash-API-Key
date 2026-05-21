dda90d8a88331807e385f576ba8312c982feee9a
panakantipranay <panakantipranay@gmail.com>
2026-05-21 16:52:37 +0530

    Update ChatController

--- src/main/java/com/SpringAI/chat/ChatController.java
+++ src/main/java/com/SpringAI/chat/ChatController.java
@@ -2,27 +2,25 @@ package com.SpringAI.chat;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/gemini")
 public class ChatController {
 
 
     private final ChatClient chatClient;
 
-    public ChatController(@Qualifier("geminiAiChatClient") ChatClient chatClient) {
-        this.chatClient = chatClient;
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
     }
 
     @GetMapping("/chat")
-    public String chat(){
+    public String chat(@RequestParam String message){
         return chatClient.prompt()
-                .user("Tell me a placement preparation tip.")
+                .user(message)
                 .call()
                 .content();
     }
