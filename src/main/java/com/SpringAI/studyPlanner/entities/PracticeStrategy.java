496dac0092a303e105df01cbf80b45180d841019
panakantipranay <panakantipranay@gmail.com>
2026-05-24 18:13:22 +0530

    Add entities for structured output

---
+++ src/main/java/com/SpringAI/studyPlanner/entities/PracticeStrategy.java
@@ -0,0 +1,52 @@
+package com.SpringAI.studyPlanner.entities;
+
+import java.util.List;
+
+public class PracticeStrategy {
+
+    private List<String> effectivePracticeMethods;
+    private List<String> revisionFrequency;
+    private List<String> mockTestsOrProjects;
+    private List<String> realWorldImplementationIdeas;
+    private List<String> theoryVsPracticeBalance;
+
+    public List<String> getEffectivePracticeMethods() {
+        return effectivePracticeMethods;
+    }
+
+    public void setEffectivePracticeMethods(List<String> effectivePracticeMethods) {
+        this.effectivePracticeMethods = effectivePracticeMethods;
+    }
+
+    public List<String> getRevisionFrequency() {
+        return revisionFrequency;
+    }
+
+    public void setRevisionFrequency(List<String> revisionFrequency) {
+        this.revisionFrequency = revisionFrequency;
+    }
+
+    public List<String> getMockTestsOrProjects() {
+        return mockTestsOrProjects;
+    }
+
+    public void setMockTestsOrProjects(List<String> mockTestsOrProjects) {
+        this.mockTestsOrProjects = mockTestsOrProjects;
+    }
+
+    public List<String> getRealWorldImplementationIdeas() {
+        return realWorldImplementationIdeas;
+    }
+
+    public void setRealWorldImplementationIdeas(List<String> realWorldImplementationIdeas) {
+        this.realWorldImplementationIdeas = realWorldImplementationIdeas;
+    }
+
+    public List<String> getTheoryVsPracticeBalance() {
+        return theoryVsPracticeBalance;
+    }
+
+    public void setTheoryVsPracticeBalance(List<String> theoryVsPracticeBalance) {
+        this.theoryVsPracticeBalance = theoryVsPracticeBalance;
+    }
+}
