d7f0befab08fbe374de28e9a9c1e7a83a3f552af
panakantipranay <panakantipranay@gmail.com>
2026-06-15 22:54:08 +0530

    Add DTO for study plan response

---
+++ src/main/java/com/springAi/studyPlanner/job/StudyPlanJobResponse.java
@@ -0,0 +1,42 @@
+package com.springAi.studyPlanner.job;
+
+import com.springAi.studyPlanner.entities.StudyPlanResponse;
+
+public class StudyPlanJobResponse {
+
+    private String jobId;
+    private JobStatus status;
+    private boolean startPreparation;
+    private int totalSubtopics;
+    private StudyPlanResponse plan;
+
+    public static StudyPlanJobResponse from(StudyPlanJob job) {
+        StudyPlanJobResponse r = new StudyPlanJobResponse();
+        r.jobId = job.getJobId();
+        r.status = job.getStatus();
+        r.startPreparation = job.isStartPreparation();
+        r.totalSubtopics = job.getTotalSubtopics();
+        r.plan = job.getPlan();
+        return r;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public JobStatus getStatus() {
+        return status;
+    }
+
+    public boolean isStartPreparation() {
+        return startPreparation;
+    }
+
+    public int getTotalSubtopics() {
+        return totalSubtopics;
+    }
+
+    public StudyPlanResponse getPlan() {
+        return plan;
+    }
+}
